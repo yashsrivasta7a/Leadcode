8f5141ef5d349b64c8708922cbfe3661718a1f6b
kartik <kartikmendiratta@9255134174@gmail.com>
2025-08-23 11:12:48 +0530

    project-start

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+     
+      
+      <p className="read-the-docs">
+       leetcode and github dashboard
+      </p>
+    </>
+  )
+}
+
+export default App
